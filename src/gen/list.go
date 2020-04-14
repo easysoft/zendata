62a5e68c6fd10d16a0320e5f0b63505e513f7233
aaron <462826@qq.com>
2020-04-14 09:02:20 +0800

    gen list

--- src/gen/list.go
+++ src/gen/list.go
@@ -6,10 +6,7 @@ import (
 	"unicode"
 )
 
-func GenerateList(field model.Field, count int, fieldMap map[string][]interface{}) {
-	//for i := 0; i < count; i++ {
-	//}
-
+func GenerateList(field model.Field, total int, fieldMap map[string][]interface{}) {
 	name := strings.TrimSpace(field.Name)
 	rang := strings.TrimSpace(field.Range)
 	prefix := field.Prefix
@@ -19,12 +16,8 @@ func GenerateList(field model.Field, count int, fieldMap map[string][]interface{
 
 	index := 0
 	for _, item := range rangeItems {
-		if index >= count {
-			break
-		}
-		if strings.TrimSpace(item) == "" {
-			continue // TODO: log
-		}
+		if index >= total { break }
+		if strings.TrimSpace(item) == "" { continue }
 
 		elemArr := strings.Split(item, "-")
 		start := elemArr[0]
@@ -33,16 +26,35 @@ func GenerateList(field model.Field, count int, fieldMap map[string][]interface{
 			end = elemArr[1]
 		}
 
-		if len(start) > 1 || len(end) > 1 {
-			continue // TODO: log
+		if len(start) > 1 || len(end) > 1 { // must be one character
+			continue
 		}
 
-		if unicode.IsDigit(rune(start[0])) && unicode.IsDigit(rune(end[0])) {
+		if (unicode.IsDigit(rune(start[0])) || unicode.IsLetter(rune(start[0]))) && // must be digit or letter
+				 (unicode.IsDigit(rune(end[0])) || unicode.IsLetter(rune(end[0]))) {
+			items := GenerateListItems(byte(start[0]), byte(end[0]), index, total)
 
-		} else if unicode.IsLetter(rune(start[0])) && unicode.IsLetter(rune(end[0])) {
-
+			fieldMap[name] = append(fieldMap[name], items)
+			index = index + len(items)
+		}
+			continue
 		}
 
 		index++
 	}
+}
+
+func GenerateListItems(startCh byte, endCh byte, index int, total int) []byte {
+	arr := make([]byte, 0)
+
+	for i := 0; i < total - index; i++ {
+		bt := startCh + byte(i)
+		if bt >= endCh {
+			break
+		}
+
+		arr = append(arr, bt)
+	}
+
+	return arr
 }
