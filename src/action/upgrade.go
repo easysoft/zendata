313e5de6d4222d636a810eac3890bbaaca553a44
aaron <462826@qq.com>
2020-05-12 13:53:18 +0800

    add 36hex seq column

--- src/action/upgrade.go
+++ src/action/upgrade.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	constant "github.com/easysoft/zendata/src/utils/const"
 	logUtils "github.com/easysoft/zendata/src/utils/log"
+	numbUtils "github.com/easysoft/zendata/src/utils/numb"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,30 +13,40 @@ import (
 func Upgrade() {
 	db, err := sql.Open(constant.SqliteDriver, constant.SqliteSource)
 	if err != nil {
-		logUtils.Screen("fail to open " + constant.SqliteSource)
+		logUtils.Screen("fail to open " + constant.SqliteSource + ": " + err.Error())
 		return
 	}
 
-	sql := "SELECT city.id, city.name, parent.name state, city.zipCode, city.cityCode " +
-			"FROM cn_city city JOIN cn_city parent ON city.parentCode = parent.areaCode"
+	sql := "SELECT id, 'name', state, zipCode, cityCode FROM city"
 	rows, err := db.Query(sql)
 	if err != nil {
-		logUtils.Screen("fail to exec query " + sql)
+		logUtils.Screen("fail to exec query " + sql + ": " + err.Error())
 		return
 	}
 
+	sqls := make([]string, 0)
 	for rows.Next() {
 		var id int
 		var name string
 		var state string
-		var zipCode int
-		var cityCode int
+		var zipCode string
+		var cityCode string
 
 		err = rows.Scan(&id, &name, &state, &zipCode, &cityCode)
 		if err != nil {
-			logUtils.Screen("fail to get sqlite3 row")
+			logUtils.Screen("fail to get sqlite3 row: " + err.Error())
 			return
 		}
-		fmt.Println(id, name, state, zipCode, cityCode)
+		fmt.Println(numbUtils.NumToBHex(id), name, state, zipCode, cityCode)
+
+		sql := fmt.Sprintf("UPDATE city SET seq = '%s' where id = %d;", numbUtils.NumToBHex(id), id)
+		sqls = append(sqls, sql)
+	}
+
+	for _, sql := range sqls {
+		_, err := db.Exec(sql)
+		if err != nil {
+			logUtils.Screen("fail to update row: " + err.Error())
+		}
 	}
 }
