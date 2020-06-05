6e6df1372e581c84c583eca6d423b84167f20f31
aaron <462826@qq.com>
2020-06-05 14:38:14 +0800

    build cmd

--- xdoc/build-mac.sh
+++ xdoc/build-mac.sh
@@ -7,11 +7,3 @@ cp -r demo build/
 go-bindata -o=res/res.go -pkg=res res/ res/doc
 
 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o build/zd-mac src/zd.go
-
-cd build
-
-cp zd-mac zd
-zip -r zd-mac-1.0.zip zd data demo
-rm zd
-
-cd ..
