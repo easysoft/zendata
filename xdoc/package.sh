e2485a0efc29ea38f963875e7b64b478d9ca312d
aaron <462826@qq.com>
2020-06-05 14:24:44 +0800

    build cmd

--- xdoc/package.sh
+++ xdoc/package.sh
@@ -1,6 +1,3 @@
-scp aaron@172.16.13.132:/home/aaron/dev/project/go/zendata/build/zd-linux* build
-scp admin@192.168.11.150:C:\dev\project\go\zendata\build\zd-* build
-
 cd build
 
 cp zd-x86.exe zd.exe
