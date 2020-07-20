c3c3bcb706ef5f1153c45b592fd96df99cf7b926
aaron <462826@qq.com>
2020-07-20 13:18:08 +0800

    update win build script

--- xdoc/2-build-win.bat
+++ xdoc/2-build-win.bat
@@ -7,9 +7,9 @@ SET CGO_ENABLED=1
 SET GOOS=windows
 
 SET GOARCH=386
-go build -o build\zd-x86.exe src/zd.go
+go build -o build\zd-x86.exe src\zd.go
 
 SET GOARCH=amd64
-go build -o build\zd-amd64.exe src/zd.go
+go build -o build\zd-amd64.exe src\zd.go
 
 scp build\zd-* aaron@172.16.13.1:/Users/aaron/rd/project/zentao/go/zd/build
