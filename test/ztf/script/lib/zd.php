5924f92b8ace16f7fc51bd255326735b5d00c158
aaron <462826@qq.com>
2020-08-06 17:24:52 +0800

    add service test scripts

--- test/ztf/script/lib/zd.php
+++ test/ztf/script/lib/zd.php
@@ -90,6 +90,49 @@ class zendata
         exec($cmdStr, $output);
     }
 
+    public function startService($port, $root)
+    {
+        $this->stopService($port);
+
+        $cmdStr = sprintf("nohup %s -p %d  >log.txt 2>&1 &", $this->cmdPath, $port);
+        if ($root) {
+            $cmdStr = str_replace(" -p ", " -R " . $root . " -p ", $cmdStr);
+        }
+
+        print("$cmdStr\n");
+
+        pclose(popen($cmdStr, 'r'));
+
+        exec($cmdStr, $output);
+
+        exec("lsof -i :8848", $output);
+        $str = join($output, "\n");
+        print("$str\n");
+    }
+    public function stopService($port)
+    {
+        $cmdStr = sprintf("kill -9 `lsof -i :%d -t`", $port);
+        print("$cmdStr\n");
+
+        exec($cmdStr, $output);
+    }
+    public function httpGet($port, $default, $conf, $lines, $options = array())
+    {
+        $url = sprintf("http://127.0.0.1:%d/?d=%s/%s&c=%s/%s&lines=%d",
+            $port, $this->workDir, $default, $this->workDir, $conf, $lines);
+
+        if (array_key_exists("root", $options)) {
+            $url .= "&root=" . $options["root"];
+        }
+
+        print("$url\n");
+
+        $resp = file_get_contents($url);
+        print("$resp\n");
+
+        return $resp;
+    }
+
     public function cmd($params)
     {
         $cmdStr = sprintf("%s %s", $this->cmdPath, $params);
---
+++ test/ztf/script/service/svc_default_config.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+<?php
+/**
+[case]
+title=
+cid=0
+pid=0
+
+[group]
+ >>
+ >>
+
+[esac]
+*/
+
+include_once __DIR__ . DIRECTORY_SEPARATOR . '../lib/zd.php';
+
+$zd = new zendata();
+
+$port = 8848;
+$zd->startService($port);
+
+$resp = $zd->httpGet($port, "default.yaml", "test.yaml", 10);
+
+$jsonArr = json_decode($resp,TRUE);
+
+$count = sprintf("%d", count($jsonArr));
+print(">> $count\n");
+
+$field = $jsonArr[2]["field_common"];
+print(">> $field\n");
+
+$zd->stopService(8848);
