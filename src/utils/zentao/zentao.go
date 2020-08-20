7e46d91ff002d3025da7b616003e778c23bf49e2
aaron <462826@qq.com>
2020-08-20 11:05:23 +0800

    implement task#7667

--- src/utils/zentao/zentao.go
+++ src/utils/zentao/zentao.go
@@ -1,19 +1,1 @@
 package zentaoUtils
-
-import (
-	"fmt"
-	constant "github.com/easysoft/zendata/src/utils/const"
-	"github.com/easysoft/zendata/src/utils/vari"
-)
-
-func GenApiUri(module string, methd string, param string) string {
-	var uri string
-
-	if vari.RequestType == constant.RequestTypePathInfo {
-		uri = fmt.Sprintf("%s-%s-%s.json", module, methd, param)
-	} else {
-		uri = fmt.Sprintf("index.php?m=%s&f=%s&%s&t=json", module, methd, param)
-	}
-
-	return uri
-}
