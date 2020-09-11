c8949b8b18aae5b1e4312caea76b86abe88728ef
aaron <462826@qq.com>
2020-09-11 10:30:20 +0800

    looking for excels in a dir

--- xdoc/3-build-linux.sh
+++
@@ -1,7 +0,0 @@
-rm -rf bin
-mkdir bin
-
-go-bindata -o=res/res.go -pkg=res res/ res/en res/zh
-
-GO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o bin/zd-linux src/zd.go
-scp bin/zd-linux* aaron@172.16.13.1:/Users/aaron/rd/project/zentao/go/zd/bin
