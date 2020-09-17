f2535e1729fdec840703d262fccdeb7a777a2009
aaron <462826@qq.com>
2020-09-17 17:40:05 +0800

    update the file-not-found msg

--- src/gen/text.go
+++ src/gen/text.go
@@ -28,8 +28,8 @@ func CreateFieldValuesFromText(field *model.DefField, fieldValue *model.FieldWit
 	realPath := findFilePath(file)
 	content, err := ioutil.ReadFile(realPath)
 	if err != nil {
-		logUtils.PrintTo(i118Utils.I118Prt.Sprintf("fail_to_read_file", realPath))
-		fieldValue.Values = append(fieldValue.Values, fmt.Sprintf("FILE_NOT_FOUND: %s", realPath))
+		logUtils.PrintTo(i118Utils.I118Prt.Sprintf("fail_to_read_file", file + " - " +  realPath))
+		fieldValue.Values = append(fieldValue.Values, fmt.Sprintf("FILE_NOT_FOUND"))
 		return
 	}
 
