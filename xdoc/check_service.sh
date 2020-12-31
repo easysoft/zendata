364cff7a53d1484ad4daa645c19913c43aaa39cf
aaron <462826@qq.com>
2020-12-31 19:19:04 +0800

    restart after upgraded

--- xdoc/check_service.sh
+++ xdoc/check_service.sh
@@ -18,6 +18,7 @@ DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 PORT=`ps -ef | grep "$PARAM_NAME" | grep -v "grep" | grep -v ".sh" | awk '{print $10}'`
 echo name: $PARAM_NAME, dir: $DIR, port $PORT
 
+# init $PARAM_PORT
 if [ -z "$PARAM_PORT" ]; then
     if [ -z "$PORT" ]; then
       if [ "$PARAM_NAME" = "ztf" ]; then
@@ -30,15 +31,16 @@ if [ -z "$PARAM_PORT" ]; then
     fi
 fi
 
+# just upgraded
 if [ -f "$DIR/.upgraded"]; then
   echo upgraded, force to restart.
-  PORT="-1" # cause service be killed
+  PORT="-1" # different port cause service be killed and restart
 fi
 
 for var in 1 2
 do
 
-  if [ -z "$PORT" ]; then # is empty, start service
+  if [ -z "$PORT" ]; then # empty, start service
 
     echo $nowTime start service on port $PARAM_PORT in dir $DIR.
     cd $DIR
@@ -50,11 +52,11 @@ do
 
   else
 
-    if [ $PORT = $PARAM_PORT ]; then
+    if [ $PORT = $PARAM_PORT ]; then # do nothing
       echo service is still alive
       echo sleep $interval second the $var time.
       sleep $interval
-    else
+    else # kill current service
       echo kill service on port $PORT.
       ps -ef | grep "$PARAM_NAME" | grep -v "grep" | grep -v ".sh" | awk '{print $2}' | xargs kill -9
       PORT="" # cause service started in the next iteration
