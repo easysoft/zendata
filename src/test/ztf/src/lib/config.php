b6e6b3369a1fcb0b2ceb6a4fcb01b4c686f827e5
陈琦 <chenqi@easycorp.ltd>
2021-03-09 13:55:34 +0800

    update demo

--- src/test/ztf/src/lib/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config['zd']['win64']     = "../../bin/zendata/1.2.0/win64/zd/zd.exe";
-$config['zd']['win32']     = "../../bin/zendata/1.2.0/win32/zd/zd.exe";
-$config['zd']['linux']     = "../../bin/zendata/1.2.0/linux/zd/zd";
-$config['zd']['mac']       = "../../bin/zendata/1.2.0/mac/zd/zd";
-
-$config['zd']['workDir']   = "../../demo";
--- src/test/ztf/src/lib/debug.php
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-if (!function_exists('simplexml_load_file')) {
-    print("simplexml_load_file missing, pls use 'sudo apt-get install php7.0-simplexml' to install\n");
-}
