713d03b725298e0e316bd50040e911f80577f840
aaron <462826@qq.com>
2021-04-09 17:26:05 +0800

    set value of protobuf class properties

--- runtime/protobuf/convert.php
+++ runtime/protobuf/convert.php
@@ -9,6 +9,23 @@ $from->setName('aaron');
 $data = $from->serializeToString();
 file_put_contents('data.bin', $data);
 
+$method = $reflect->getMethods();
+foreach ($method as $key_m => $value_m) {
+    $methodName = $value_m->getName();
+    $found = strpos($methodName, 'set');
+    if ($found !== false) {
+//        var_dump($methodName);
+
+        $comments = $value_m->getDocComment();
+//        var_dump($comments);
+
+        // <code>.Address address = 4;</code>
+        $pattern = '/<code>\.?(.+?)\s/is';
+        preg_match($pattern, $comments, $match);
+        echo $match[1] . "\n";
+    }
+}
+
 // test
 $data = file_get_contents('data.bin');
 $to = new ${cls_name}();
