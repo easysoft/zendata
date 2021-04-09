23385d82ddd2c5b660b5757b1b2fedc55624ced1
aaron <462826@qq.com>
2021-04-09 16:50:58 +0800

    close task #37353,37355

---
+++ runtime/protobuf/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'aaron/test',
+  ),
+  'versions' => 
+  array (
+    'aaron/test' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'google/protobuf' => 
+    array (
+      'pretty_version' => 'v3.15.7',
+      'version' => '3.15.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '96b613b0de4a96ef423404ea893b19e7e2cfe474',
+    ),
+  ),
+);
