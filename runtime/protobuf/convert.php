155d8c6d53a6579f0990ac39ddf033fe67cd87f8
aaron <462826@qq.com>
2021-04-12 12:15:58 +0800

    set class values

--- runtime/protobuf/convert.php
+++ runtime/protobuf/convert.php
@@ -6,37 +6,47 @@ include 'GPBMetadata/Runtime/Protobuf/${cls_name}.php';
 $typeArr = array("double","float","int32","int64","uint32","uint64","sint32","sint64",
     "fixed32","fixed64","sfixed32","sfixed64","bool","string","bytes");
 
-$from = new ${cls_name}();
-$from->setName('aaron');
+$inst = new ${cls_name}();
+$inst->setName('aaron');
 
-$reflect = new ReflectionObject($from);
-$method = $reflect->getMethods();
-foreach ($method as $key => $value) {
+$reflect = new ReflectionObject($inst);
+$methods = $reflect->getMethods();
+foreach ($methods as $key => $value) {
     $methodName = $value->getName();
     $found = strpos($methodName, 'set');
     if ($found !== false) {
         $repeated = false;
         $className = "";
 
-        getProps($value, $repeated, $className);
-        printClsField($from, $repeated, $className, $methodName);
+        parserFieldPropsFromComments($value, $repeated, $className);
+        setFieldDefaultValue($inst, $repeated, $className, $methodName);
     }
 }
 
-$data = $from->serializeToString();
+$data = $inst->serializeToString();
 file_put_contents('data.bin', $data);
 
-function printClsField($inst, $repeated, $className, $methodName)
+$data = file_get_contents('data.bin');
+$decode = new Person();
+$decode->mergeFromString($data);
+
+print_r(json_encode($decode));
+
+function setFieldDefaultValue(&$inst, $repeated, $className, $methodName)
 {
     global $typeArr;
 
     $ref = new ReflectionObject($inst);
-    echo "object           = " . $ref->getName() . "\n";
+    $objectType = $ref->getName();
+    echo "object type      = $objectType\n";
     echo "field type       = $className\n";
     echo "field method     = $methodName\n";
     echo "field repeated   = $repeated\n\n";
 
-    if (in_array($className, $typeArr)) {
+    if (isStandType($className)) {
+        $defaultVal = getDefaultValByType($className, $repeated);
+        call_user_func(array($inst, $methodName), $defaultVal);
+
         return;
     }
 
@@ -54,15 +64,15 @@ function printClsField($inst, $repeated, $className, $methodName)
 
         $repeated = false;
         $fieldClassName = "";
-        getProps($value, $repeated, $fieldClassName);
+        parserFieldPropsFromComments($value, $repeated, $fieldClassName);
 
-        if (!in_array($fieldClassName, $typeArr)) {
-            printClsField($propObj, $repeated, $fieldClassName, $methodName);
+        if (!isStandType($fieldClassName)) {
+            setFieldDefaultValue($propObj, $repeated, $fieldClassName, $methodName);
         }
     }
 }
 
-function getProps($value, &$repeated, &$className)
+function parserFieldPropsFromComments($value, &$repeated, &$className)
 {
     $comments = $value->getDocComment();
     // <code>.Address address = 4;</code>
@@ -84,10 +94,92 @@ function getProps($value, &$repeated, &$className)
     $className = trim($className);
 }
 
-// test
-//
-//$data = file_get_contents('data.bin');
-//$to = new Person();
-//$to->mergeFromString($data);
-//
-//echo $to->getName() . PHP_EOL;
+function getDefaultValByType($type, $repeat) {
+    if (!$repeat) {
+        return getRandValByType($type);
+    }
+
+    $count = rand(3, 30);
+    $ret = array();
+    for ($i = 0; $i < $count; $i++) {
+        $item = getRandValByType($type);
+        $ret[$i] = $item;
+    }
+
+    return $ret;
+}
+
+function getRandValByType($type) {
+    if ($type === 'bool') { // : string
+        $ret = rand(0, 1);
+        return $ret;
+
+    } else if ($type === 'string') { // : string
+        $r = rand(3, 100);
+        $ret = getRandStr($r);
+        return $ret;
+
+    } else if ($type === 'float') { // java float: float
+        $start = pow(2,7) * -1;
+        $end = pow(2,7) - 1;
+
+        $ret = getRandFloat($start, $end);
+        return $ret;
+
+    } else if ($type === 'double') { // java double: float
+        $start = pow(2,10) * -1;
+        $end = pow(2,10) - 1;
+
+        $ret = getRandFloat($start, $end);
+        return $ret;
+
+    } else if ($type === 'int32' || $type === 'sint32' || $type === 'sfixed32') { // go int32 : integer
+        $start = pow(2,31) * -1;
+        $end = pow(2,31) - 1; // 2147483647
+
+        $ret = rand($start, $end);
+        return $ret;
+
+    } else if ($type === 'uint32' || $type === 'fixed32') { // go uint32 : integer
+        $end = pow(2,32) - 1; // 4294967295
+
+        $ret = rand(0, $end);
+        return $ret;
+
+    } else if ($type === 'int64' || $type === 'sint64' || $type === 'sfixed64') { // go int64 : integer/string
+        $ret = rand() << 32 | rand(); //    6571882023217245969
+        //   -3451450498452162931
+        $sign = rand(0, 1);
+        if ($sign == 0) {
+            $ret *= -1;
+        }
+        return $ret;
+
+    } else if ($type === 'uint64' || $type === 'fixed64') { // go uint64 : integer
+        $ret = rand() << 32 | rand(); //    8252018705439509776
+        $ret = ($ret - 1) * 2;
+        return $ret;
+
+    }
+}
+
+function isStandType($className) {
+    global $typeArr;
+
+    return in_array($className, $typeArr);
+}
+
+function getRandStr($length = 10) {
+    srand(date("s"));
+    $chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $string = "";
+    while(strlen($string) < $length) {
+        $string .= substr($chars,(rand()%(strlen($chars))),1);
+    }
+    return($string);
+}
+
+function getRandFloat($min = 0, $max = 1) {
+    $rl = mt_rand() / mt_getrandmax();
+    return ($min + ($rl * ($max - $min)));
+}
