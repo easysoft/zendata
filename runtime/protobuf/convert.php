c140a9d2a4837d429db5cb1a2e7a1361b5b9842e
aaron <462826@qq.com>
2021-04-12 16:48:49 +0800

    set property value and print

--- runtime/protobuf/convert.php
+++ runtime/protobuf/convert.php
@@ -3,8 +3,8 @@ include '../vendor/autoload.php';
 include '${cls_name}.php';
 include 'GPBMetadata/Runtime/Protobuf/${cls_name}.php';
 
-$typeArr = array("double","float","int32","int64","uint32","uint64","sint32","sint64",
-    "fixed32","fixed64","sfixed32","sfixed64","bool","string","bytes");
+$typeArr = array("double", "float", "int32", "int64", "uint32", "uint64", "sint32", "sint64",
+    "fixed32", "fixed64", "sfixed32", "sfixed64", "bool", "string", "bytes");
 
 $inst = new ${cls_name}();
 
@@ -20,7 +20,7 @@ foreach ($methods as $key => $value) {
         $setMethodParamCls = "";
 
         parserFieldPropsFromComments($value, $repeated, $setMethodParamCls);
-        setFieldDefaultValue($inst, $repeated, $setMethodParamCls, $setMethodName);
+        setFieldDefaultValue($inst, $repeated, $setMethodName, $setMethodParamCls);
     }
 }
 
@@ -32,102 +32,50 @@ $decode = new Person();
 $decode->mergeFromString($data);
 
 printSep("print object");
-printObj($decode);
+$prefix = 0;
+printObj($decode, $prefix);
 
-function printObj($obj) {
-    $reflect = new ReflectionObject($obj);
-    $name = $reflect->getName();
-    $methods = $reflect->getMethods();
-    foreach ($methods as $key => $value) {
-        $methodName = $value->getName();
-        $found = strpos($methodName, 'get');
-        if ($found !== false && $methodName !== "getClass" && $methodName !== "getType" && $methodName !== "getLegacyClass") {
-            $repeated = false;
-            $className = "";
-            parserFieldPropsFromComments($value, $repeated, $className);
-            $var = call_user_func(array($obj, $methodName));
-
-            if (isStandType($className) || gettype($var) === 'integer') {
-                printField($name, $methodName, $var, $repeated, $className);
-                continue;
-            }
-
-            printObj($var);
-        }
-    }
-}
-
-function printField($name, $methodName, $var, $repeated, $className) {
-    if ($className === 'bool') {
-        if ($var) $var = 'true';
-        else $var = 'false';
-    }
-
-    if (!$repeated) {
-        $name = $name . '->' . substr($methodName, 3);
-        $name = str_pad($name, 26," ", STR_PAD_RIGHT);
-        echo "$name = $var\n";
-        return;
-    }
-
-    $arr = array();
-    foreach ($var as $key => $value) {
-        $arr[] = $value;
-    }
-
-    $name = str_pad($name . '->' . substr($methodName, 3), 26," ", STR_PAD_RIGHT);
-    echo "$name = [" . join(",",$arr) . "] \n";
-}
-
-function setFieldDefaultValue(&$inst, $repeated, $setMethodParamCls, $setMethodName)
+function setFieldDefaultValue(&$parentObj, $repeated, $setMethodName, $setMethodParamCls)
 {
-    global $typeArr;
-
-    $ref = new ReflectionObject($inst);
-    $objectType = $ref->getName();
-    echo "object type      = $objectType\n";
+    $parentReflect = new ReflectionObject($parentObj);
+    $parentObjType = $parentReflect->getName();
+    echo "object type      = $parentObjType\n";
     echo "field type       = $setMethodParamCls\n";
     echo "field method     = $setMethodName\n";
     echo "field repeated   = $repeated\n\n";
 
     if (isStandType($setMethodParamCls)) { //
         $defaultVal = getDefaultValByType($setMethodParamCls, $repeated);
-        call_user_func(array($inst, $setMethodName), $defaultVal);
+        call_user_func(array($parentObj, $setMethodName), $defaultVal);
 
         return;
     }
 
     require "./$setMethodParamCls.php";
 
-    $child = new $setMethodParamCls();
-    $reflect = new ReflectionObject($child);
-    $methods = $reflect->getMethods();
+    $childObj = new $setMethodParamCls();
+    $childReflect = new ReflectionObject($childObj);
+    $childMethods = $childReflect->getMethods();
     $isEnum = true;
-    foreach ($methods as $key => $value) {
-        $setMethodName2 = $value->getName();
-        $found = strpos($setMethodName2, 'set');
+    foreach ($childMethods as $key => $value) {
+        $childSetMethodName = $value->getName();
+        $found = strpos($childSetMethodName, 'set');
         if ($found === false) {
             continue;
         }
 
-        $isEnum = false;
+        $isEnum = false; // enum has no set method
 
-        $repeated2 = 0;
-        $fieldClassName = "";
-        parserFieldPropsFromComments($value, $repeated2, $fieldClassName);
-        setFieldDefaultValue($child, $repeated2, $fieldClassName, $setMethodName2);
+        $childRepeated = 0;
+        $childSetMethodParamCls = "";
+        parserFieldPropsFromComments($value, $childRepeated, $childSetMethodParamCls);
+        setFieldDefaultValue($childObj, $childRepeated, $childSetMethodName, $childSetMethodParamCls);
     }
 
-    if (!$isEnum) {
-        $children = $child;
-        if ($repeated) {
-            $children = array();
-            $children[] = $child;
-        }
+    if ($isEnum) return;
 
-        call_user_func(array($inst, $setMethodName), $children);
-    }
-
+    $children = $repeated? array($childObj) : $childObj;
+    call_user_func(array($parentObj, $setMethodName), $children);
 }
 
 function parserFieldPropsFromComments($value, &$repeated, &$className)
@@ -152,7 +100,8 @@ function parserFieldPropsFromComments($value, &$repeated, &$className)
     $className = trim($className);
 }
 
-function getDefaultValByType($type, $repeat) {
+function getDefaultValByType($type, $repeat)
+{
     if (!$repeat) {
         return getRandValByType($type);
     }
@@ -167,7 +116,8 @@ function getDefaultValByType($type, $repeat) {
     return $ret;
 }
 
-function getRandValByType($type) {
+function getRandValByType($type)
+{
     if ($type === 'bool') { // : string
         $ret = rand(0, 1);
         return $ret;
@@ -178,28 +128,28 @@ function getRandValByType($type) {
         return $ret;
 
     } else if ($type === 'float') { // java float: float
-        $start = pow(2,7) * -1;
-        $end = pow(2,7) - 1;
+        $start = pow(2, 7) * -1;
+        $end = pow(2, 7) - 1;
 
         $ret = getRandFloat($start, $end);
         return $ret;
 
     } else if ($type === 'double') { // java double: float
-        $start = pow(2,10) * -1;
-        $end = pow(2,10) - 1;
+        $start = pow(2, 10) * -1;
+        $end = pow(2, 10) - 1;
 
         $ret = getRandFloat($start, $end);
         return $ret;
 
     } else if ($type === 'int32' || $type === 'sint32' || $type === 'sfixed32') { // go int32 : integer
-        $start = pow(2,31) * -1;
-        $end = pow(2,31) - 1; // 2147483647
+        $start = pow(2, 31) * -1;
+        $end = pow(2, 31) - 1; // 2147483647
 
         $ret = rand($start, $end);
         return $ret;
 
     } else if ($type === 'uint32' || $type === 'fixed32') { // go uint32 : integer
-        $end = pow(2,32) - 1; // 4294967295
+        $end = pow(2, 32) - 1; // 4294967295
 
         $ret = rand(0, $end);
         return $ret;
@@ -221,27 +171,95 @@ function getRandValByType($type) {
     }
 }
 
-function isStandType($className) {
+function printObj($obj, $prefix)
+{
+    $reflect = new ReflectionObject($obj);
+    $name = $reflect->getName();
+    echo str_repeat(' ', $prefix) . "[$name]\n";
+
+    if ($name === 'Google\Protobuf\Internal\RepeatedField') {
+        foreach ($obj as $key => $value) {
+            printObj($value, $prefix + 3);
+        }
+    }
+    if ($name)
+
+        $methods = $reflect->getMethods();
+    foreach ($methods as $key => $value) {
+        $methodName = $value->getName();
+        $found = strpos($methodName, 'get');
+        if ($found !== false && $methodName !== "getClass" && $methodName !== "getType" && $methodName !== "getLegacyClass"
+            && $methodName !== "getIterator") {
+            if ($methodName === 'getAddressItems') {
+                echo '';
+            }
+
+            $repeated = false;
+            $className = "";
+            parserFieldPropsFromComments($value, $repeated, $className);
+            $var = call_user_func(array($obj, $methodName));
+
+            if (isStandType($className) || gettype($var) === 'integer') {
+                printField($methodName, $var, $repeated, $className, $prefix + 3);
+                continue;
+            }
+
+            $name = str_repeat(' ', $prefix + 3) . substr($methodName, 3);
+            $name = str_pad($name, 26, " ", STR_PAD_RIGHT);
+            echo "$name\n";
+            printObj($var, $prefix + 3);
+        }
+    }
+}
+
+function printField($methodName, $var, $repeated, $className, $prefix)
+{
+    if ($className === 'bool') {
+        if ($var) $var = 'true';
+        else $var = 'false';
+    }
+
+    if (!$repeated) {
+        $name = str_repeat(' ', $prefix) . substr($methodName, 3);
+        $name = str_pad($name, 26, " ", STR_PAD_RIGHT);
+        echo "$name = $var\n";
+        return;
+    }
+
+    $arr = array();
+    foreach ($var as $key => $value) {
+        $arr[] = $value;
+    }
+
+    $name = str_pad(str_repeat(' ', $prefix) . substr($methodName, 3), 26, " ", STR_PAD_RIGHT);
+    echo "$name = [" . join(",", $arr) . "] \n";
+}
+
+function isStandType($className)
+{
     global $typeArr;
 
     return in_array($className, $typeArr);
 }
 
-function getRandStr($length = 10) {
+function getRandStr($length = 10)
+{
     srand(date("s"));
     $chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $string = "";
-    while(strlen($string) < $length) {
-        $string .= substr($chars,(rand()%(strlen($chars))),1);
+    while (strlen($string) < $length) {
+        $string .= substr($chars, (rand() % (strlen($chars))), 1);
     }
-    return($string);
+    return ($string);
 }
 
-function getRandFloat($min = 0, $max = 1) {
+function getRandFloat($min = 0, $max = 1)
+{
     $rl = mt_rand() / mt_getrandmax();
     return ($min + ($rl * ($max - $min)));
 }
 
-function printSep($title) {
-    echo(str_repeat("=",16) . " " . $title . " " . str_repeat("=",16) . "\n");
+function printSep($title)
+{
+    echo(str_repeat("=", 16) . " " . $title . " " . str_repeat("=", 16) . "\n");
 }
