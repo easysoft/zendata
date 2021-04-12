347ebfec4019bc1046c382ff67b26046dc74ce47
aaron <462826@qq.com>
2021-04-12 09:56:28 +0800

    reflect to print all class field properties

--- runtime/protobuf/convert.php
+++ runtime/protobuf/convert.php
@@ -3,55 +3,91 @@ include '../vendor/autoload.php';
 include '${cls_name}.php';
 include 'GPBMetadata/Runtime/Protobuf/${cls_name}.php';
 
+$typeArr = array("double","float","int32","int64","uint32","uint64","sint32","sint64",
+    "fixed32","fixed64","sfixed32","sfixed64","bool","string","bytes");
+
 $from = new ${cls_name}();
 $from->setName('aaron');
+
+$reflect = new ReflectionObject($from);
+$method = $reflect->getMethods();
+foreach ($method as $key => $value) {
+    $methodName = $value->getName();
+    $found = strpos($methodName, 'set');
+    if ($found !== false) {
+        $repeated = false;
+        $className = "";
+
+        getProps($value, $repeated, $className);
+        printClsField($from, $repeated, $className, $methodName);
+    }
+}
 
 $data = $from->serializeToString();
 file_put_contents('data.bin', $data);
 
-$reflect = new ReflectionObject($from);
-//$props = $reflect->getProperties();
-//foreach ($props as $key_p => $value_p) {
-//    var_dump($value_p->getName());
-//}
+function printClsField($inst, $repeated, $className, $methodName)
+{
+    global $typeArr;
 
-$method = $reflect->getMethods();
-foreach ($method as $key_m => $value_m) {
-    $methodName = $value_m->getName();
-    $found = strpos($methodName, 'set');
-    if ($found !== false) {
-//        var_dump($methodName);
+    $ref = new ReflectionObject($inst);
+    echo "object           = " . $ref->getName() . "\n";
+    echo "field type       = $className\n";
+    echo "field method     = $methodName\n";
+    echo "field repeated   = $repeated\n\n";
 
-        $comments = $value_m->getDocComment();
-//        var_dump($comments);
+    if (in_array($className, $typeArr)) {
+        return;
+    }
 
-        // <code>.Address address = 4;</code>
-        $pattern = '/<code>\.?(.+?)\s/is';
-        preg_match($pattern, $comments, $match);
+    require "./$className.php";
 
-        $className = $match[1];
-        echo "$className\n";
+    $propObj = new $className();
+    $reflect = new ReflectionObject($propObj);
+    $methods = $reflect->getMethods();
+    foreach ($methods as $key => $value) {
+        $methodName = $value->getName();
+        $found = strpos($methodName, 'set');
+        if ($found === false) {
+            continue;
+        }
 
-        if (!in_array($className, array("string", "int32"))) {
-            require "./$className.php";
+        $repeated = false;
+        $fieldClassName = "";
+        getProps($value, $repeated, $fieldClassName);
 
-            $propObj = new $className();
-            $reflect2 = new ReflectionObject($propObj);
-            $methods2 = $reflect2->getMethods();
-            foreach ($methods2 as $key_m2 => $value_m2) {
-                $methodName2 = $value_m2->getName();
-                $found2 = strpos($methodName2, 'set');
-                if ($found2 !== false) {
-                    echo $methodName2 . "\n";
-                }
-            }
+        if (!in_array($fieldClassName, $typeArr)) {
+            printClsField($propObj, $repeated, $fieldClassName, $methodName);
         }
     }
 }
 
-// test
-$data = file_get_contents('data.bin');
-$to = new ${cls_name}();
-$to->mergeFromString($data);
+function getProps($value, &$repeated, &$className)
+{
+    $comments = $value->getDocComment();
+    // <code>.Address address = 4;</code>
+    $pattern = '/<code>(repeated\s)?\.?(.+?)\s/is';
+    preg_match($pattern, $comments, $match);
+    if (sizeof($match) >= 3) {
+        $repeated = $match[1];
+        $className = $match[2];
+    } else if (sizeof($match) >= 2) {
+        $repeated = false;
+        $className = $match[1];
+    }
+    $repeated = trim($repeated);
+    if ($repeated === 'repeated')
+        $repeated = 1;
+    else
+        $repeated = 0;
 
-echo $to->getName() . PHP_EOL;
+    $className = trim($className);
+}
+
+// test
+//
+//$data = file_get_contents('data.bin');
+//$to = new Person();
+//$to->mergeFromString($data);
+//
+//echo $to->getName() . PHP_EOL;
