ff5d80574cf4dd1c75c3d422f77da227c330a988
hind3ight <1157595693@qq.com>
2021-12-03 19:17:46 +0800

    对数据库字段定义的支持

--- src/action/sql.go
+++ src/action/sql.go
@@ -45,26 +45,29 @@ func ParseSql(file string, out string) {
 	for tableName, statement := range statementMap {
 		createStr := statement
 
-		columns := getColumnsFromCreateStatement(createStr)
+		columns := getColumnsFromCreateStatement2(createStr)
+		// todo
+		// handle fieldTypes
+		//rans := getFieldType(infos)
 
 		def := model.DefSimple{}
 		def.Init(tableName, "automated export", "", "1.0")
 
-		for _, col := range columns {
-			field := model.FieldSimple{Field: col}
+		for m, col := range columns {
+			field := model.FieldSimple{Field: m}
 
 			// pk
-			isPk := col == pkMap[tableName]
-			fkInfo, isFk := fkMap[col]
+			isPk := m == pkMap[tableName]
+			fkInfo, isFk := fkMap[m]
 
 			if isPk {
 				field.From = "keys.yaml"
-				field.Use = fmt.Sprintf("%s_%s", tableName, col)
+				field.Use = fmt.Sprintf("%s_%s", tableName, m)
 			} else if isFk {
 				field.From = "keys.yaml"
 				field.Use = fmt.Sprintf("%s_%s{:1}", fkInfo[0], fkInfo[1])
 			} else {
-				field.Range = "-"
+				field.Range = col
 			}
 
 			def.Fields = append(def.Fields, field)
@@ -150,4 +153,196 @@ func getColumnsFromCreateStatement(sent string) []string {
 	}
 
 	return fieldLines
+}
+
+func getColumnsFromCreateStatement2(sent string) (fieldInfo map[string]string) {
+	//fieldLines := make([]string, 0)
+	re := regexp.MustCompile("(?iU)\\s*(\\S+)\\s.*\n")
+	arr := re.FindAllStringSubmatch(string(sent), -1)
+	//fieldTypes := make([]string, 0)
+	fieldInfo = map[string]string{}
+	for _, item := range arr {
+		line := strings.ToLower(item[0])
+		if !strings.Contains(line, " table ") && !strings.Contains(line, " key ") {
+			field := item[1]
+			fieldType := strings.Split(strings.TrimSpace(strings.Split(strings.Split(item[0], field)[1], ",")[0]), " ")[0]
+			//fieldTypes = append(fieldTypes, fieldType)
+			field = strings.ReplaceAll(field, "`", "")
+			fieldInfo[field] = judge(fieldType)
+			//fieldLines = append(fieldLines, field)
+		}
+	}
+
+	return fieldInfo
+}
+
+func judge(fieldType string) (ran string) {
+	ran = ""
+	typ := strings.ToUpper(strings.Split(fieldType, "(")[0])
+	switch typ {
+	// value type
+	case "TINYINT":
+		ran = "-128-127"
+		fmt.Println("TINYINT")
+	case "SMALLINT":
+		ran = "-32768-32767"
+		fmt.Println("SMALLINT")
+	case "MEDIUMINT":
+		ran = "-8388608~8388607"
+		fmt.Println("MEDIUMINT")
+	case "INT", "INTEGER":
+		ran = "-2147483648~2147483647"
+		fmt.Println("INT/INTEGER")
+	case "FLOAT":
+		ran = "123.457"
+		fmt.Println("FLOAT")
+	case "BIGINT":
+		ran = "BIGINT"
+		fmt.Println("BIGINT")
+	case "DOUBLE":
+		ran = "DOUBLE"
+		fmt.Println("DOUBLE")
+	case "DECIMAL":
+		ran = "DECIMAL"
+		fmt.Println("DECIMAL")
+
+	// Date and time type
+	case "DATE":
+		ran = "2008-12-2"
+		fmt.Println("DATE")
+	case "TIME":
+		ran = "12:25:36"
+		fmt.Println("TIME")
+	case "YEAR":
+		ran = "1901/2155"
+		fmt.Println("YEAR")
+	case "DATETIME":
+		ran = "20210101 000000-20210101 230000:60"
+		fmt.Println("DATETIME")
+	case "TIMESTAMP":
+		ran = "1970-01-01 00:00:00/2038"
+		fmt.Println("TIMESTAMP")
+	// String type
+	case "CHAR":
+		ran = "CHAR"
+		fmt.Println("CHAR")
+	case "VARCHAR":
+		ran = "VARCHAR"
+		fmt.Println("VARCHAR")
+	case "TINYBLOB":
+		ran = "TINYBLOB"
+		fmt.Println("TINYBLOB")
+	case "TINYTEXT":
+		ran = "TINYTEXT"
+		fmt.Println("TINYTEXT")
+	case "BLOB":
+		ran = "BLOB"
+		fmt.Println("BLOB")
+	case "TEXT":
+		ran = "TEXT"
+		fmt.Println("TEXT")
+	case "MEDIUMBLOB":
+		ran = "MEDIUMBLOB"
+		fmt.Println("MEDIUMBLOB")
+	case "MEDIUMTEXT":
+		ran = "MEDIUMTEXT"
+		fmt.Println("MEDIUMTEXT")
+	case "LONGBLOB":
+		ran = "LONGBLOB"
+		fmt.Println("LONGBLOB")
+	case "LONGTEXT":
+		ran = "LONGTEXT"
+		fmt.Println("LONGTEXT")
+	default:
+		ran = "0-255"
+		fmt.Println("other types")
+	}
+	return ran
+}
+
+func getFieldType(fieldTypes []string) (rans []string) {
+	ran := "0-255"
+	for _, v := range fieldTypes {
+		typ := strings.ToUpper(strings.Split(v, "(")[0])
+		switch typ {
+		// value type
+		case "TINYINT":
+			ran = "-128-127"
+			fmt.Println("TINYINT")
+		case "SMALLINT":
+			ran = "-32768-32767"
+			fmt.Println("SMALLINT")
+		case "MEDIUMINT":
+			ran = "-8388608~8388607"
+			fmt.Println("MEDIUMINT")
+		case "INT", "INTEGER":
+			ran = "-2147483648~2147483647"
+			fmt.Println("INT/INTEGER")
+		case "FLOAT":
+			ran = "123.457"
+			fmt.Println("FLOAT")
+		case "BIGINT":
+			ran = "BIGINT"
+			fmt.Println("BIGINT")
+		case "DOUBLE":
+			ran = "DOUBLE"
+			fmt.Println("DOUBLE")
+		case "DECIMAL":
+			ran = "DECIMAL"
+			fmt.Println("DECIMAL")
+
+		// Date and time type
+		case "DATE":
+			ran = "2008-12-2"
+			fmt.Println("DATE")
+		case "TIME":
+			ran = "12:25:36"
+			fmt.Println("TIME")
+		case "YEAR":
+			ran = "1901/2155"
+			fmt.Println("YEAR")
+		case "DATETIME":
+			ran = "20210101 000000-20210101 230000:60"
+			fmt.Println("DATETIME")
+		case "TIMESTAMP":
+			ran = "1970-01-01 00:00:00/2038"
+			fmt.Println("TIMESTAMP")
+		// String type
+		case "CHAR":
+			ran = "CHAR"
+			fmt.Println("CHAR")
+		case "VARCHAR":
+			ran = "VARCHAR"
+			fmt.Println("VARCHAR")
+		case "TINYBLOB":
+			ran = "TINYBLOB"
+			fmt.Println("TINYBLOB")
+		case "TINYTEXT":
+			ran = "TINYTEXT"
+			fmt.Println("TINYTEXT")
+		case "BLOB":
+			ran = "BLOB"
+			fmt.Println("BLOB")
+		case "TEXT":
+			ran = "TEXT"
+			fmt.Println("TEXT")
+		case "MEDIUMBLOB":
+			ran = "MEDIUMBLOB"
+			fmt.Println("MEDIUMBLOB")
+		case "MEDIUMTEXT":
+			ran = "MEDIUMTEXT"
+			fmt.Println("MEDIUMTEXT")
+		case "LONGBLOB":
+			ran = "LONGBLOB"
+			fmt.Println("LONGBLOB")
+		case "LONGTEXT":
+			ran = "LONGTEXT"
+			fmt.Println("LONGTEXT")
+		default:
+			ran = "0-255"
+			fmt.Println("other types")
+		}
+		rans = append(rans, ran)
+	}
+	return rans
 }
