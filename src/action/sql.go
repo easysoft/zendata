39e41f4c41c923c4e8471cc49159a0f7716c1304
Hind3ight <83194637+Hind3ight@users.noreply.github.com>
2021-12-06 16:51:23 +0800

    Add support for database field definition (#25)
    * 除整型外，其余字段range属性使用双引号包括
    
    * 字符串类型、时间类型生成与默认配置相符
    
    *  field_test.yaml为示例文件

--- src/action/sql.go
+++ src/action/sql.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -45,7 +46,7 @@ func ParseSql(file string, out string) {
 	for tableName, statement := range statementMap {
 		createStr := statement
 
-		columns := getColumnsFromCreateStatement(createStr)
+		columns, types := getColumnsFromCreateStatement2(createStr)
 
 		def := model.DefSimple{}
 		def.Init(tableName, "automated export", "", "1.0")
@@ -64,15 +65,24 @@ func ParseSql(file string, out string) {
 				field.From = "keys.yaml"
 				field.Use = fmt.Sprintf("%s_%s{:1}", fkInfo[0], fkInfo[1])
 			} else {
-				field.Range = "-"
+				if types[col] == `"DATE"` || types[col] == `"TIME"` || types[col] == `"YEAR"` || types[col] == `"DATETIME"` || types[col] == `"TIMESTAMP"` {
+					field.Range = strconv.Quote("20210821 000000:60")
+					field.Type = "timestamp"
+					field.Format = strconv.Quote("YY/MM/DD hh:mm:ss")
+				} else if types[col] == `"CHAR"` || types[col] == `"VARCHAR"` || types[col] == `"TINYTEXT"` || types[col] == `"TEXT"` || types[col] == `"MEDIUMTEXT"` || types[col] == `"LONGTEXT"` {
+					field.Range = types[col]
+					field.Loop = "'3'" // default value of loop
+					field.Loopfix = "_"
+				} else {
+					field.Range = types[col]
+				}
 			}
 
 			def.Fields = append(def.Fields, field)
 		}
 
 		bytes, _ := yaml.Marshal(&def)
-		content := strings.ReplaceAll(string(bytes), "'-'", "\"\"")
-
+		content := strings.ReplaceAll(string(bytes), "'", "")
 		if out != "" {
 			out = fileUtils.AddSepIfNeeded(out)
 			outFile := out + tableName + ".yaml"
@@ -150,4 +160,155 @@ func getColumnsFromCreateStatement(sent string) []string {
 	}
 
 	return fieldLines
+}
+
+func getColumnsFromCreateStatement2(sent string) (fieldLines []string, fieldInfo map[string]string) {
+	fieldLines = make([]string, 0)
+	re := regexp.MustCompile("(?iU)\\s*(\\S+)\\s.*\n")
+	arr := re.FindAllStringSubmatch(string(sent), -1)
+	fieldInfo = map[string]string{}
+	for _, item := range arr {
+		line := strings.ToLower(item[0])
+		if !strings.Contains(line, " table ") && !strings.Contains(line, " key ") {
+			fieldTmp := item[1]
+			field := strings.ReplaceAll(fieldTmp, "`", "")
+			fieldInfo[field] = parseFieldInfo(strings.ToUpper(fieldTmp), item[0])
+			fieldLines = append(fieldLines, field)
+		}
+	}
+
+	return fieldLines, fieldInfo
+}
+
+func parseFieldInfo(fieldTmp, fieldTypeInfo string) (ran string) {
+	var ret []string
+	isUnsigned := false
+	fieldTypeInfo = strings.ToUpper(fieldTypeInfo)
+	if strings.Contains(fieldTypeInfo, "UNSIGNED") { // judge unsigned
+		isUnsigned = true
+	}
+
+	myExp := regexp.MustCompile(fieldTmp + `\s([A-Z]+)\(([^,]*),?([^,]*)\)`)
+	ret = myExp.FindStringSubmatch(fieldTypeInfo)
+	if ret != nil {
+		ran = judgeFieldType(ret[1], ret[2], isUnsigned)
+	} else {
+		fieldType := strings.Split(strings.Fields(fieldTypeInfo)[1], "(")[0]
+		ran = judgeFieldType(fieldType, "", isUnsigned)
+	}
+
+	return ran
+}
+
+func judgeFieldType(fieldType, num string, isUnsigned bool) (ran string) {
+	switch fieldType {
+	// integer
+	case "BIT":
+		if isUnsigned {
+			ran = "0-255"
+		} else {
+			ran = "-128-127"
+		}
+	case "TINYINT":
+		if num == "1" {
+			ran = "0-1"
+		} else if isUnsigned {
+			ran = "0-255"
+		} else {
+			ran = "-128-127"
+		}
+	case "SMALLINT":
+		if isUnsigned {
+			ran = "0-65535"
+		} else {
+			ran = "-32768-32767"
+		}
+	case "MEDIUMINT":
+		if isUnsigned {
+			ran = "0-65535"
+		} else {
+			ran = "-32768-32767"
+		}
+	case "INT", "INTEGER":
+		if isUnsigned {
+			ran = "[0,2^32-1]"
+		} else {
+			ran = "[-2^31,2^31-1]"
+		}
+	case "BIGINT":
+		if isUnsigned {
+			ran = "[0,2^64-1]"
+		} else {
+			ran = "[-2^63 ,2^63 -1]"
+		}
+	// floating-point
+	case "FLOAT":
+		ran = `"FLOAT"`
+	case "DOUBLE":
+		ran = `"DOUBLE"`
+	// fixed-point
+	case "DECIMAL":
+		ran = `"DECIMAL"`
+	// character string
+	case "CHAR":
+		ran = `"CHAR"`
+	case "VARCHAR":
+		ran = `"VARCHAR"`
+	case "TINYTEXT":
+		ran = `"TINYTEXT"`
+	case "TEXT":
+		ran = `"TEXT"`
+	case "MEDIUMTEXT":
+		ran = `"MEDIUMTEXT"`
+	case "LONGTEXT":
+		ran = `"LONGTEXT"`
+	// binary data
+	case "TINYBLOB":
+		ran = `"TINYBLOB"`
+	case "BLOB":
+		ran = `"BLOB"`
+	case "MEDIUMBLOB":
+		ran = `"MEDIUMBLOB"`
+	case "LONGBLOB":
+		ran = `"LONGBLOB"`
+	case "BINARY":
+		ran = "BINARY"
+	case "VARBINARY":
+		ran = "VARBINARY"
+	// Date and time type
+	case "DATE":
+		ran = `"DATE"`
+	case "TIME":
+		ran = `"TIME"`
+	case "YEAR":
+		ran = `"YEAR"`
+	case "DATETIME":
+		ran = `"DATETIME"`
+	case "TIMESTAMP":
+		ran = `"TIMESTAMP"`
+	// other type
+	case "ENUM":
+		ran = `"ENUM"`
+	case "SET":
+		ran = `"SET"`
+	case "GEOMETRY":
+		ran = `"GEOMETRY"`
+	case "POINT":
+		ran = `"POINT"`
+	case "LINESTRING":
+		ran = `"LINESTRING"`
+	case "POLYGON":
+		ran = `"POLYGON"`
+	case "MULTIPOINT":
+		ran = `"MULTIPOINT"`
+	case "MULTILINESTRING":
+		ran = `"MULTILINESTRING"`
+	case "MULTIPOLYGON":
+		ran = `"MULTIPOLYGON"`
+	case "GEOMETRYCOLLECTION":
+		ran = `"GEOMETRYCOLLECTION"`
+	default:
+	}
+
+	return ran
 }
