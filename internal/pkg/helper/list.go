ba2b8a4a48c8ecab08fe8bab69ed92e874c5c444
aaron <462826@qq.com>
2022-12-02 10:31:35 +0800

    fix a sample yaml parser error

--- internal/pkg/helper/list.go
+++ internal/pkg/helper/list.go
@@ -12,7 +12,6 @@ import (
 	"github.com/mattn/go-runewidth"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 	"regexp"
 	"sort"
@@ -171,10 +170,6 @@ func GetFilesAndDirs(pth, typ string, res *map[string][]model.ResFile) {
 }
 
 func ReadYamlInfo(path string) (title, desc, resType string) {
-	if strings.Index(path, "01_range.yaml") > -1 {
-		log.Print("")
-	}
-
 	info := model.DefInfo{}
 
 	if strings.Index(path, "apache") > -1 {
