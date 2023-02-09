4414f572fb913546243fa27e4d30b88a1d4ac766
aaron <462826@qq.com>
2023-02-09 11:02:07 +0800

    fix an issue about create values from literal

--- internal/pkg/model/domain.go
+++ internal/pkg/model/domain.go
@@ -97,7 +97,7 @@ type FieldSimple struct {
 	Field        string `yaml:"field,omitempty"`
 	Note         string `yaml:"note,omitempty"`
 	Range        string `yaml:"range,omitempty"`
-	RangeLiteral string `yaml:"RangeLiteral,omitempty" json:"-"`
+	RangeLiteral string `yaml:"RangeLiteral,omitempty" json:"omitempty"`
 	Value        string `yaml:"value,omitempty"`
 	Prefix       string `yaml:"prefix,omitempty"`
 	Postfix      string `yaml:"postfix,omitempty"`
